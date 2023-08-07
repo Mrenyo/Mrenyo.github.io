3863e0f12dbfdff8156c639da316d054009ffe40
unknown <tankwar05@freemail.hu>
2023-08-07 12:07:26 +0200

    Javascript

---
+++ JavaScript/animation.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.item-1');
+hiddenElements.forEach((el) => observer.observe(el));
